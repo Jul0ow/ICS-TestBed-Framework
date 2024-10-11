3aa3c30d9c8d64590506db9cfd97c4a26d353c0f
Matthieu <matthieu.colin@epita.fr>
2024-10-11 19:03:21 +0200

    start progression plc
    Signed-off-by: Matthieu <matthieu.colin@epita.fr>

--- node/src/main/java/xyz/scada/testbed/node/Main.java
+++ node/src/main/java/xyz/scada/testbed/node/Main.java
@@ -10,6 +10,7 @@ import org.springframework.shell.standard.ShellOption;
 import xyz.scada.testbed.node.hmi.HMI;
 import xyz.scada.testbed.node.hmi.exceptions.PlcAlreadyPresent;
 
+import java.io.Console;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -68,13 +69,16 @@ public class Main {
     // TODO hmi to Modbus
 
     @ShellMethod(value = "Add a new plc to the hmi", group = "MHI", prefix = "")
-    public void addPlc(@ShellOption() String name, @ShellOption() String ipAddr, @ShellOption(defaultValue = "502") String port, @ShellOption(defaultValue = "") String description) {
+    public void addPlc(@ShellOption() String name, @ShellOption() String ipAddr,
+                       @ShellOption(defaultValue = "502") String port,
+                       @ShellOption(defaultValue = "plc", help = "supported type are plc (default one), progression") String type,
+                       @ShellOption(defaultValue = "") String description) {
         if (hmi == null)
             hmi = new HMI();
 
         try {
-            hmi.addPlc(name, ipAddr, Integer.parseInt(port), description);
-        } catch (PlcAlreadyPresent e) {
+            hmi.addPlc(name, ipAddr, Integer.parseInt(port), type, description);
+        } catch (Exception e) {
             System.err.println("Failed to add the plc: " + e.getMessage());
         }
     }
@@ -151,6 +155,19 @@ public class Main {
             System.err.println("Failed to read coils: " + e.getMessage());
         }
     }
+
+    @ShellMethod(value = "Gets the checkpoint from the given progression Plc", group = "MHI", prefix = "")
+    public void getCheckpoints(@ShellOption() String name) {
+        if (hmi == null)
+            hmi = new HMI();
+
+        try {
+            System.out.println(hmi.getCheckpoints(name));
+        } catch (Exception e) {
+            System.err.println("Failed to get checkpoints: " + e.getMessage());
+        }
+    }
+
 
     // TODO check for historian
 
